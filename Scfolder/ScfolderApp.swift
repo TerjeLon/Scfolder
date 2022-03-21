625089c4b3bb6e06d1aa40b3ee3aefc50d3172ad
Terje Lønøy <terje@scelto.no>
2022-03-21 09:04:36 +0100

    Initial Commit

---
+++ Scfolder/ScfolderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScfolderApp.swift
+//  Scfolder
+//
+//  Created by Terje Lønøy on 21/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct ScfolderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
